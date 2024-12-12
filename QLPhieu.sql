cb2d966ae83dc7da710064d448e566cd066b90bf
tieudinh <120320511+tieudinh@users.noreply.github.com>
2024-12-12 21:37:11 +0700

    Add files via upload

---
+++ QLPhieu.sql
@@ -0,0 +1,79 @@
+﻿create database QLTV;
+use QLPhieu
+
+create table TheLoai(
+MaTheLoai varchar(6) primary key, --Ma The Loai bat dau bang chu "TL" 
+TenTheLoai nvarchar(100)
+);
+select * from TheLoai
+insert into TheLoai(MaTheLoai, TenTheLoai) values
+('TL1','Tình cảm'),
+('TL2','Trinh thám'),
+('TL3','Kinh dị'),
+('TL4','Viễn tưởng'),
+('TL5','Giáo dục');
+
+create table Sach(
+MaSach varchar(6) primary key, -- Ma Sach bat dau bang ki tu "S"
+TenSach nvarchar(255),
+MaTheLoai varchar(6) foreign key (MaTheLoai) references TheLoai(MaTheLoai) on delete cascade,
+TacGia nvarchar(255), 
+NhaXuatBan nvarchar(255),
+NamXuatBan int
+);
+
+create table DocGia(
+MaDocGia varchar(6) primary key, --Ma Doc gia bat dau bang DG
+HoTen nvarchar(255),
+Username nvarchar(10) unique,
+Password nvarchar(10) unique,
+NgaySinh date, 
+SoDienThoai nvarchar(255),
+Email nvarchar(255),
+DiaChi nvarchar(255)
+)
+
+select * from DocGia
+insert into DocGia(MaDocGia, HoTen, NgaySinh, SoDienThoai, Email, DiaChi, Username, Password) values
+('DG1', 'Nguyen Van A', '01-01-2000', '01111111111', 'anguyenvan@gmail.com','280 An Duong Vuong, P4, Q5, TPHCM','anv123', '01012000')
+
+create table NhanVien(
+MaNhanVien varchar(6) primary key, --Ma Nhan vien bat dau bang chu NV
+HoTen nvarchar(255),
+NgaySinh date,
+Username nvarchar(10) unique,
+Password nvarchar(10) unique,
+SoDienThoai nvarchar(255),
+Email nvarchar(255),
+DiaChi nvarchar(255)
+)
+
+insert into NhanVien(MaNhanVien, HoTen, NgaySinh, Username, Password, Email, SoDienThoai, DiaChi)values
+('NV1', 'Tran Van B', '01-01-1999', 'btranvan12', '123456', 'tranvanb123@gmail.com', '0222222222', '222 Le Van Sy, Q3')
+
+
+create table GiaoDichMuonSach(
+MaTheMuon varchar(6) primary key, --Ma the muon bat dau bang chu TM
+MaDocGia varchar(6) foreign key (MaDocGia) references DocGia(MaDocGia) on delete cascade,
+MaSach varchar(6) foreign key (MaSach) references Sach(MaSach) on delete cascade,
+NgayMuon date,
+NgayTraDuKien date,
+NgayTraThucTe date,
+TrangThai nvarchar(255)
+)
+
+select * from GiaoDichMuonSach
+insert into GiaoDichMuonSach(MaTheMuon, MaDocGia, MaSach, NgayMuon, NgayTraDuKien, NgayTraThucTe, TrangThai)values
+('TM1', 'DG1', 'S1', '2024-12-20', '2024-12-27', '2024-12-30', 'Chua Tra')
+
+
+
+create table TienPhat( -- Khong co Khoa chinh
+MaTheMuon varchar(6) foreign key (MaTheMuon) references GiaoDichMuonSach(MaTheMuon),
+SoTienPhat real,
+TrangThaiThanhToan nvarchar(255)
+);
+select * from TienPhat
+insert into TienPhat(MaTheMuon, SoTienPhat, TrangThaiThanhToan)values
+('TM1', 3*10000, 'Chua Thanh Toan')
+
---
+++ SQLQuery1 (1).sql
@@ -0,0 +1,87 @@
+﻿create database QLThuVien
+drop database QLThuVien
+use QLThuVien
+
+create table Sach(
+MaSach nchar(10) primary key not null,
+TenSach nvarchar(255),
+TacGia nvarchar(255),
+TheLoai nvarchar(255),
+NhaXuatBan nvarchar(255),
+GiaSach int,
+TinhTrang nvarchar(255))
+
+
+create table DocGia(
+MaDocGia nchar(10) primary key not null,
+TenDangNhap nchar(10) unique,
+HoTen nvarchar(255),
+GioiTinh nvarchar(10),
+NamSinh int,
+DiaChi nvarchar(255),
+SDT nvarchar(255))
+
+create table TaiKhoan(
+MaDocGia nchar(10) foreign key (MaDocGia) references DocGia(MaDocGia) on delete cascade,
+TenDangNhap nchar(10) not null unique,
+MatKhau nchar(10) not null unique,
+VaiTro int)
+
+create table PhieuMuon(
+MaPhieu nchar(10) primary key not null,
+MaDocGia nchar(10) foreign key (MaDocGia) references DocGia(MaDocGia) on delete cascade,
+MaSach nchar(10) foreign key (MaSach) references Sach(MaSach) on delete cascade,
+NgayMuon datetime,
+NgayPhaiTra datetime)
+
+create table PhieuTra(
+MaPhieu nchar(10) primary key not null,
+MaDocGia nchar(10) foreign key (MaDocGia) references DocGia(MaDocGia) on delete cascade,
+MaSach nchar(10) foreign key (MaSach) references Sach(MaSach) on delete cascade,
+NgayTra datetime)
+
+insert into Sach(MaSach, TenSach, TacGia, TheLoai, NhaXuatBan, GiaSach, TinhTrang) values
+('MS01', 'Cong Nghe Thong Tin I','Nguyen Van A', 'Khoa Học', 'GDDDT','70000','Còn'),
+('MS02', 'Thong Tin II','Nguyen Van B', 'Giáo Dục', 'TPHCM','80000','Còn'),
+('MS03', 'Cong Nghe ','Lam Vy A', 'Tự nhiên', 'DHSP','90000','Còn'),
+('MS04', 'Cong Nghe Thong Tin VI','Tran Thanh T', 'Khoa Học', 'KT','50000','Hết');
+
+insert into DocGia(MaDocGia, TenDangNhap, HoTen, GioiTinh, NamSinh, DiaChi, SDT) values
+('DG1', '23van2', 'Nguyen Van A', 'Nam','2004', '280 An Duong Vuong', '0121012000'),
+('DG2', 'uyen3van2', 'Uuyen Van', 'Nam','2004', 'Q5, TPHCM', '0121012666'),
+('DG3', 'nguyen22', 'Nguyen A', 'Nữ','2006', 'Q11', '0121011457'),
+('DG4', 'uyen23van2', 'Van Anh', 'Nữ','2005', 'KTX', '0121012250');
+
+
+insert into TaiKhoan(MaDocGia, TenDangNhap, MatKhau, VaiTro) values
+('DG1', '23van2','123', '1'),
+('DG2', 'uyen3van2','456', '1'),
+('DG3', 'nguyen22','789', '2'),
+('DG4', 'uyen23van2','666', '2');
+
+insert into PhieuMuon(MaPhieu, MaDocGia, MaSach, NgayMuon, NgayPhaiTra) values
+('MP01', 'DG1', 'MS01', '2024-12-20', '2024-12-30'),
+('MP011', 'DG2', 'MS01', '2024-12-10', '2024-12-20'),
+('MP03', 'DG3', 'MS02', '2024-12-20', '2024-12-30'),
+('MP033', 'DG1', 'MS03', '2024-12-20', '2024-12-30'),
+('MP02', 'DG2', 'MS01', '2024-12-15', '2024-12-25');
+
+insert into PhieuTra(MaPhieu, MaDocGia, MaSach,NgayTra) values
+('MP01', 'DG1', 'MS01','2024-12-30'),
+('MP011', 'DG2', 'MS01', '2024-12-20'),
+('MP03', 'DG1', 'MS02', '2024-12-30'),
+('MP033', 'DG1', 'MS03', '2024-12-30'),
+('MP02', 'DG1', 'MS01', '2024-12-30');
+
+select * from PhieuMuon;
+SELECT * FROM DocGia WHERE MaDocGia = 'DG2';
+delete from PhieuMuon where MaPhieu = 'MP033' 
+
+select * from PhieuMuon where MaPhieu = 'MP02' 
+SELECT * FROM Sach WHERE MaSach = 'MP02';
+select * from PhieuTra;
+
+SELECT * FROM Sach WHERE MaSach = 'MP06';
+SELECT * FROM DocGia WHERE MaDocGia = 'DG1';
+INSERT INTO Sach (MaSach, 1) VALUES ('MP06', 3);
+INSERT INTO DocGia (MaDocGia, 2) VALUES ('DG1', 3);
